225413a2cd671479bd00c265aba3e16fd48fc9e0
Kristin Baumann <mail@kristin-baumann.com>
2019-09-28 16:00:22 +0200

    Added basic content structure and first styling with fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,46 @@
 import React from "react"
 
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
 
 import "../styles/layout.css"
+import "typeface-nunito"
+import "typeface-libre-baskerville"
+import "../styles/custom.css"
 
 const IndexPage = () => (
-  <div>
-    <SEO title="Home" />
-    <h1>Hi, I'm Kristin Baumann.</h1>
-    <p>
-      I love to create meaningful things on the web, pragmatically and
-      interdisciplinarly:
-    </p>
-    <ul>
-      <li>Frontend Development</li>
-      <li>Product Management</li>
-      <li>UX Design</li>
-      <li>Data Visualisation</li>
-    </ul>
-    <div>
-      <p>Looking for help?</p>
-      <button>Get in contact</button>
+  <div class="container">
+    {/* <SEO title="Home" /> */}
+    <h1 class="headline">
+      Hi, I'm <strong>Kristin Baumann</strong>.
+    </h1>
+    <div class="section-content">
+      <p>
+        I love to create meaningful things on the web, pragmatically and
+        interdisciplinarly:
+      </p>
+      <ul class="topics">
+        <li>- Frontend Development</li>
+        <li>- Product Management</li>
+        <li>- UX Design</li>
+        <li>- Data Visualisation</li>
+      </ul>
     </div>
-    <ul>
-      <li>LinkedIn</li>
-      <li>Twitter</li>
+    <div class="section-cta">
+      <p>Looking for help?</p>
+      <button class="cta" href="mailto:test@test.de">
+        Get in contact
+      </button>
+    </div>
+    <ul class="section-interaction links">
+      <li>
+        <a href="#">LinkedIn</a>
+      </li>
+      <li>
+        <a href="#">Twitter</a>
+      </li>
+      <li>
+        <a href="#">Github</a>
+      </li>
     </ul>
   </div>
 )
